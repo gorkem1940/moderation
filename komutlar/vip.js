7f9a1141bb4997a6dc0bf8de2eb1d33efcae0316
gorkem <113816295+gorkem1940@users.noreply.github.com>
2023-05-10 11:03:22 +0300

    Update vip.js

--- komutlar/vip.js
+++ komutlar/vip.js
@@ -3,21 +3,21 @@ const db = require('croxydb');
 const ayarlar = require("../ayarlar.json");
 
 exports.run = async(client, message, args) => {
-   if (!message.member.roles.cache.has("849341898245275688","849341802653024296","849351388857499658") && !message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(new Discord.MessageEmbed().addField("Yetersiz Yetki",` Bu komutu kullanabilmen için gerekli yetkin yok.`).setColor("RANDOM")).then(m => m.delete({timeout: 7000}));
+   if (!message.member.roles.cache.has("849341898245275688") && !message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(new Discord.MessageEmbed().addField("Yetersiz Yetki",` Bu komutu kullanabilmen için gerekli yetkin yok.`).setColor("RANDOM")).then(m => m.delete({timeout: 7000}));
    let member = message.mentions.users.first() || client.users.cache.get(args.join(' '))
    if(!member) {
        return message.channel.send('\・Bir kişi etiketlemelisin.')
    }
-   let tanrısızvip = message.guild.roles.cache.find(r => r.id === '848127316680638505') // Rol id
+   let gorkemvip = message.guild.roles.cache.find(r => r.id === '848127316680638505') // Rol id
 
-   if(!tanrısızvip) {
+   if(!gorkemvip) {
        return message.channel.send('Hata oluştu!')
    }
 
    let vipa = message.guild.member(member)
 
 
-   vipa.roles.add(tanrısızvip)
+   vipa.roles.add(gorkemvip)
    let RİA = new Discord.MessageEmbed()
    .setColor('BLACK')
    .setTitle('<a:sseytan:841782368771768350> VIP ROLU VERİLDİ. <a:sseytan:841782368771768350>')
